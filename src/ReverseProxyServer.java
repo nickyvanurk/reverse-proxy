6b6859d4588fa42737974e89a2c5da53216fd725
Nicky van Urk <nickyvanurk@gmx.com>
2020-04-26 17:44:08 +0200

    Add basic server structure

--- src/ReverseProxyServer.java
+++ src/ReverseProxyServer.java
@@ -4,8 +4,35 @@ import java.util.concurrent.*;
 import java.util.logging.*;
 
 public class ReverseProxyServer {
+  private static final Logger logger = Logger.getLogger(
+      ReverseProxyServer.class.getCanonicalName());
+  private final int port;
+
+  public ReverseProxyServer(int port) throws IOException {
+    this.port = port;
+  }
+
+  public void start() throws IOException {
+    logger.log(Level.INFO, "Reverse proxy server starting");
+  }
+
   public static void main(String[] args) {
-    MyClass myObj = new MyClass();
-    System.out.println(myObj.x);
+    int port;
+    try {
+      port = Integer.parseInt(args[0]);
+
+      if (port < 0 || port > 65535) {
+        port = 8080;
+      }
+    } catch (RuntimeException ex) {
+      port = 8080;
+    }
+
+    try {
+      ReverseProxyServer reverseProxyServer = new ReverseProxyServer(port);
+      reverseProxyServer.start();
+    } catch (IOException ex) {
+      logger.log(Level.SEVERE, "Reverse proxy server could not start", ex);
+    }
   }
 }
