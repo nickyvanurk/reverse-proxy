f3b18a07559a1360eefa97fd11e1af56235f4182
Nicky van Urk <nickyvanurk@gmx.com>
2020-04-26 16:21:12 +0200

    And so it begins...

---
+++ README.md
@@ -0,0 +1,7 @@
+# Reverse Proxy
+
+A reverse proxy written in Java for a college networking course.
+
+## License
+
+This project is licensed under the [MIT License](./LICENSE).
---
+++ src/ReverseProxyServer.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
+import java.util.logging.*;
+
+public class ReverseProxyServer {
+  public static void main(String[] args) {
+    MyClass myObj = new MyClass();
+    System.out.println(myObj.x);
+  }
+}
